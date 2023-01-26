73dd51bae82b56334bdbb3ec0413c4a248807246
Sandro Dornelas <sandrodornelas.sd@gmail.com>
2023-01-26 00:49:52 -0300

    Update TesteDataContext.cs

--- Teste/Data/TesteDataContext.cs
+++ Teste/Data/TesteDataContext.cs
@@ -9,7 +9,7 @@ namespace Teste.Data
       
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Password=Dornelas;Persist Security Info=True;User ID=sa;Initial Catalog=Db_Processo;Data Source=DORNELAS;Trust Server Certificate=true");
+            optionsBuilder.UseSqlServer("CONNECTION-STRING");
         }
     }
 }
